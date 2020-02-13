82b90a06b0ef763e1385c4b586c349c263953cd3
Giovani Aguirre <me@its-g.io>
2020-02-13 14:57:50 -0600

    Added massive and middleware on index.js

--- server/index.js
+++ server/index.js
@@ -4,9 +4,20 @@ const session = require("express-session");
 const massive = require("massive");
 require("dotenv").config();
 
-const { SERVER_PORT, CONNECTION_STRING } = process.env;
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+
+massive(CONNECTION_STRING)
+  .then(db => app.set("db", db))
+  .catch(err => console.error(err));
 
 app
   .use(express.json())
+  .use(
+    session({
+      secret: SESSION_SECRET,
+      saveUninitialized: true,
+      resave: false,
+    })
+  )
 
 app.listen(SERVER_PORT, () => console.log(`Rodger Rodger on port ${SERVER_PORT}`))
