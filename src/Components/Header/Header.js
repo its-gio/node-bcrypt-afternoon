ed56bef5d6a6e236a2dc29acfc5582f11a38bb87
Giovani Aguirre <me@its-g.io>
2020-02-13 17:12:30 -0600

    Register functionality now available in front-end

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios"
 import './Header.css';
 
 export default class Header extends Component {
@@ -33,6 +34,13 @@ export default class Header extends Component {
 
   register() {
     // axios POST to /auth/register here
+    axios
+      .post("/auth/register", this.state)
+      .then(() => this.setState({ username: '', password: '' }))
+      .catch(err => {
+        this.setState({ username: '', password: '' });
+        alert(err.response.request.response);
+      });
   }
 
   logout() {
