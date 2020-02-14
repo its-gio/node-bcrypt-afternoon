253e3c013c9cd0568fa61523b25ac06c26a57f84
Giovani Aguirre <me@its-g.io>
2020-02-13 18:30:41 -0600

    Logout functionality available in front-end

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -52,6 +52,10 @@ export default class Header extends Component {
 
   logout() {
     // axios GET to /auth/logout here
+    axios
+      .get("/auth/logout")
+      .then(() => this.props.updateUser({}))
+      .catch(err => console.error(err));
   }
 
   render() {
